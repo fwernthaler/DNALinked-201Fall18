ac16fa918b89a6b6d332402729e8b03b987ba500
Florian Wernthaler <flw5@duke.edu>
2018-10-24 13:25:41 -0400

    Made constructors

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,5 +1,51 @@
-//Florian Wernthaler
-//Jordan McGilvery
-public class LinkStrand {
+public class LinkStrand implements IDnaStrand {
+	public LinkStrand(String s) {
+		initialize(s);
+	}
+	
+	public LinkStrand() {
+		this("");
+	}
+	
+	@Override
+	public long size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
+	@Override
+	public void initialize(String source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IDnaStrand getInstance(String source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDnaStrand append(String dna) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDnaStrand reverse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getAppendCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public char charAt(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
