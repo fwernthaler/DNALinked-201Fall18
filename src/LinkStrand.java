5137a4e6b7b4b770518ad5a11f5b31f8da8101f2
Florian Wernthaler <flw5@duke.edu>
2018-10-25 16:27:07 -0400

    Finished LinkStrand methods up to .reverse

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,24 +1,39 @@
 public class LinkStrand implements IDnaStrand {
+	private class Node {
+		String info;
+		Node next;
+		public Node(String s) {
+			info = s;
+			next = null;
+		}
+	}
+	
+	private Node myFirst, myLast;
+	private long mySize;
+	private int myAppends;
+
 	public LinkStrand(String s) {
 		initialize(s);
 	}
-	
+
 	public LinkStrand() {
 		this("");
 	}
 	
 	@Override
-	public long size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public void initialize(String source) {
-		// TODO Auto-generated method stub
-		
+		Node firstnode = new Node(source);
+		myFirst = firstnode;
+		myLast = myFirst;
+		mySize = source.length();
+		myAppends = 0;
 	}
-
+	
+	@Override
+	public long size() {
+		return mySize;
+	}
+	
 	@Override
 	public IDnaStrand getInstance(String source) {
 		// TODO Auto-generated method stub
@@ -27,25 +42,58 @@ public class LinkStrand implements IDnaStrand {
 
 	@Override
 	public IDnaStrand append(String dna) {
-		// TODO Auto-generated method stub
-		return null;
+		Node newnode = new Node(dna);
+		myLast.next = newnode;
+		myLast = newnode;
+		mySize = mySize + dna.length();
+		myAppends++;
+		return this;
 	}
 
 	@Override
 	public IDnaStrand reverse() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder copy = new StringBuilder(myFirst.info);
+		copy.reverse();
+		LinkStrand reversed = new LinkStrand(copy.toString());
+		
+		Node n = myFirst;
+		n = n.next;
+		Node nextnode = reversed.myFirst;
+		
+		while (n != null) {
+			StringBuilder reversetext = new StringBuilder(n.info);
+			n.info = reversetext.reverse().toString();
+			
+			reversed.myFirst = n;
+			reversed.myFirst.next = nextnode;
+			nextnode = n;
+			n = n.next;
+		}
+		
+		return reversed;
 	}
 
 	@Override
 	public int getAppendCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return myAppends;
 	}
 
 	@Override
 	public char charAt(int index) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		Node n = myFirst;
+		StringBuilder build = new StringBuilder();
+		
+		while (n != null) {
+			build.append(n.info);
+			n = n.next;
+		}
+		
+		return build.toString();
 	}
 }
